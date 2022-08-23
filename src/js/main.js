f3923920ae9818380488dd943aebe66a09f4c673
Diego Oviedo <diovite111@gmail.com>
2022-08-23 15:42:21 -0500

    modificación a la sección de profile y conóceme

--- src/js/main.js
+++ src/js/main.js
@@ -1,37 +1,45 @@
-const links = document.querySelectorAll('.navbar__menu--link');
+const links = document.querySelectorAll(".navbar__menu--link");
 
 const options = {
-  rootMargin: "-50% 0px -50% 0px"
-}
+  rootMargin: "-50% 0px -50% 0px",
+};
 
-const observer = new IntersectionObserver((entries)=>{
-  entries.forEach((entry)=>{
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
     let id = entry.target.getAttribute("id");
-    let link = document.querySelector(`a[href="#${id}"]`)
-    if(entry.isIntersecting){
-      document.querySelector(".navbar__menu--link--icon.active").classList.remove('active');
-      document.querySelector(".navbar__menu--link--text.active").classList.remove('active');
+    let link = document.querySelector(`a[href="#${id}"]`);
+    if (entry.isIntersecting) {
+      document
+        .querySelector(".navbar__menu--link--icon.active")
+        .classList.remove("active");
+      document
+        .querySelector(".navbar__menu--link--text.active")
+        .classList.remove("active");
       link.childNodes[1].classList.add("active");
       link.childNodes[3].classList.add("active");
     }
   });
 }, options);
 
-links.forEach((link)=>{
-  link.addEventListener('click',(e)=>{
-    document.querySelector(".navbar__menu--link--icon.active").classList.remove('active');
-    document.querySelector(".navbar__menu--link--text.active").classList.remove('active');
+links.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    document
+      .querySelector(".navbar__menu--link--icon.active")
+      .classList.remove("active");
+    document
+      .querySelector(".navbar__menu--link--text.active")
+      .classList.remove("active");
     e.target.childNodes[1].classList.add("active");
-    e.target.childNodes[3].classList.add('active');
+    e.target.childNodes[3].classList.add("active");
   });
   const hash = link.getAttribute("href");
   const target = document.querySelector(hash);
-  if(target){
-    observer.observe(target)
+  if (target) {
+    observer.observe(target);
   }
 });
 
-const loadingDocument = ()=>{
+const loadingDocument = () => {
   document.getElementById("o").classList.toggle("loading__img--o");
   document.getElementById("d").classList.toggle("loading__img--d");
   document.getElementById("soporte").classList.toggle("loading__img--soporte");
@@ -41,7 +49,7 @@ const loadingDocument = ()=>{
 
 let idInterval = setInterval(loadingDocument, 2550);
 
-window.onload = ()=>{
+window.onload = () => {
   clearInterval(idInterval);
   document.getElementById("loading").style.display = "none";
   document.getElementById("header").removeAttribute("style");
@@ -50,3 +58,18 @@ window.onload = ()=>{
   document.getElementById("proyect").removeAttribute("style");
   document.getElementById("contact").removeAttribute("style");
 };
+
+const golang = document.getElementById("golang");
+const golangMessage = document.querySelector(".profile__golang--message");
+
+golang.addEventListener("touchstart", (e) => {
+  e.preventDefault();
+  e.style = "transform: translateX(-100%) scale(.9)";
+  golangMessage.classList.toggle("showMessage");
+});
+
+golang.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.style = "transform: translateX(-100%) scale(.9)";
+  golangMessage.classList.toggle("showMessage");
+});
